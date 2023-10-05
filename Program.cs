74cf7b6a685aa8290935cc23fe3e65f73f596bb7
Babar071 <144986706+Babar071@users.noreply.github.com>
2023-10-05 19:43:09 +0500

    10 methods of data
    addition, subtraction, multiplication. division. factorial, even number, prime number, taking square , taking cube, sentence.

--- Program.cs
+++ Program.cs
@@ -1,145 +1,112 @@
 ﻿using System;
 
-namespace ConsoleApplication5
+namespace _10_methods_of_data
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        
+    // Method 1: Addition
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Method 2: Subtraction
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    // Method 3: Multiplication
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    // Method 4: Division 
+    public static int Divide(int a, int b)
+    {
+        if (b != 0)
         {
-            string name1 = "Hassan";
-            int age1 = 10;
-            float average1 = 2.6f;
-            bool pass1 = true;
-            char grade1 = 'c';
-            
-            Console.WriteLine("Name: " + name1);
-            Console.WriteLine("Age: " + age1);
-            Console.WriteLine("Average: " + average1);
-            Console.WriteLine("Pass: " + true);
-            Console.WriteLine("Grade: " + 'c');
-            Console.WriteLine();
-
-            string name2 = "Saad";
-            int age2 = 12;
-            float average2 = 1.7f;
-            bool pass2 = false;
-            char grade2 = 'd';
-            
-            Console.WriteLine("Name: " + name2);
-            Console.WriteLine("Age: " + age2);
-            Console.WriteLine("Average: " + average2);
-            Console.WriteLine("Pass: " + false);
-            Console.WriteLine("Grade: " + 'd');
-            Console.WriteLine();
-
-            string name3 = "Atif";
-            int age3 = 14;
-            float average3 = 2.9f;
-            bool pass3 = true;
-            char grade3 = 'b';
-            
-            Console.WriteLine("Name: " + name3);
-            Console.WriteLine("Age: " + age3);
-            Console.WriteLine("Average: " + average3);
-            Console.WriteLine("Pass: " + true);
-            Console.WriteLine("Grade: " + 'b');
-            Console.WriteLine();
-
-            string name4 = "Hashim";
-            int age4 = 16;
-            float average4 = 2.1f;
-            bool pass4 = false;
-            char grade4 = 'c';
-            
-            Console.WriteLine("Name: " + name4);
-            Console.WriteLine("Age: " + age4);
-            Console.WriteLine("Average: " + average4);
-            Console.WriteLine("Pass: " + false);
-            Console.WriteLine("Grade: " + 'c');
-            Console.WriteLine(); 
-
-            string name5 = "Muzamil";
-            int age5 = 18;
-            float average5 = 3.5f;
-            bool pass5 = true;
-            char grade5 = 'a';
-            
-            Console.WriteLine("Name: " + name5);
-            Console.WriteLine("Age: " + age5);
-            Console.WriteLine("Average: " + average5);
-            Console.WriteLine("Pass: " + true);
-            Console.WriteLine("Grade: " + 'a');
-            Console.WriteLine();
-
-            string name6 = "Danish";
-            int age6 = 20;
-            float average6 = 1.2f;
-            bool pass6 = false;
-            char grade6 = 'f';
-            
-            Console.WriteLine("Name: " + name6);
-            Console.WriteLine("Age: " + age6);
-            Console.WriteLine("Average: " + average6);
-            Console.WriteLine("Pass: " + false);
-            Console.WriteLine("Grade: " + 'f');
-            Console.WriteLine();
-
-            string name7 = "Daniyal";
-            int age7 = 22;
-            float average7 = 3.1f;
-            bool pass7 = true;
-            char grade7 = 'a';
-            
-            Console.WriteLine("Name: " + name7);
-            Console.WriteLine("Age: " + age7);
-            Console.WriteLine("Average: " + average7);
-            Console.WriteLine("Pass: " + true);
-            Console.WriteLine("Grade: " + 'a');
-            Console.WriteLine();
-
-            string name8 = "Ali";
-            int age8 = 24;
-            float average8 = 1.9f;
-            bool pass8 = false;
-            char grade8 = 'd';
-            
-            Console.WriteLine("Name: " + name8);
-            Console.WriteLine("Age: " + age8);
-            Console.WriteLine("Average: " + average8);
-            Console.WriteLine("Pass: " + false);
-            Console.WriteLine("Grade: " + 'd');
-            Console.WriteLine();
-
-            string name9 = "Mushahid";
-            int age9 = 26;
-            float average9 = 2.8f;
-            bool pass9 = true;
-            char grade9 = 'b';
-            
-            Console.WriteLine("Name: " + name9);
-            Console.WriteLine("Age: " + age9);
-            Console.WriteLine("Average: " + average9);
-            Console.WriteLine("Pass: " + true);
-            Console.WriteLine("Grade: " + 'b');
-            Console.WriteLine();
-
-            string name10 = "Ibrahim";
-            int age10 = 28;
-            float average10 = 2.2f;
-            bool pass10 = false;
-            char grade10 = 'c';
-            
-             
-            Console.WriteLine("Name: " + name10);
-            Console.WriteLine("Age: " + age10);
-            Console.WriteLine("Average: " + average10);
-            Console.WriteLine("Pass: " + false);
-            Console.WriteLine("Grade: " + 'c');
-            Console.WriteLine();
-
-
-            // Result 
-            
+            return a / b;
+        }
+        
+        else
+        {
+            Console.WriteLine("You can not divide by zero,");
+            return 0;
         }
     }
+
+    // Method 5: Factorial
+    public static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    // Method 6: Even number
+    public static bool EvenorNot(int num)
+    {
+        return num / 2 == 0;
+    }
+
+    // Method 7: Prime number
+    public static bool IsPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num / i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Method 8: Taking Square
+    public static int Square(int num)
+    {
+        return num * num;
+    }
+
+    // Method 9: Taking Cube
+    public static int Cube(int num)
+    {
+        return num * num * num;
+    }
+
+    // Method 10: Sentence
+    public static void Statement(string name)
+    {
+        Console.WriteLine("My name is " + name + "!");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Addition: " + Add(5, 3));
+        Console.WriteLine("Subtraction: " + Subtract(8, 2));
+        Console.WriteLine("Multiplication: " + Multiply(4, 6));
+        Console.WriteLine("Division: " + Divide(10, 2));
+        Console.WriteLine("Factorial of 5: " + Factorial(5));
+        Console.WriteLine("Is 7 even? " + EvenorNot(7));
+        Console.WriteLine("Is 11 prime? " + IsPrime(11));
+        Console.WriteLine("Square of 4: " + Square(4));
+        Console.WriteLine("Cube of 3: " + Cube(3));
+        Statement("Babar");
+    }
 }
+
+    }
